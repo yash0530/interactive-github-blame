ff4a2aaebc8b2ce2208fdcd6a8a8b33fd33b6597
Aliabbas Merchant <merchantaliabbas@gmail.com>
2019-02-11 16:20:47 +0530

    Monitor URL change

--- content.js
+++ content.js
@@ -1,24 +1,27 @@
-let latestUrl = window.location.href;
-
-setInterval(() => {
-
-    if (latestUrl === window.location.href && 
-        window.location.href.indexOf("/blob/") !== -1) {
-
-        latestUrl = window.location.href;
-
+let URL = window.location.href
+let latestUrl = window.location.href
+function changed() {
+    if (window.location.href.indexOf("/blob/") !== -1) {
         console.log("REACHED");
-        
         // getting the html of blame page 
         const url = window.location.href.replace("/blob/", "/blame/");
-
         fetch(url)
             .then(data => data.text())
-            .then(data => {
-                console.log(data);
-                
-                // TODO
+            .then(html => {
+                // console.log(html);
+                console.log(document.querySelector("body")).appendChild(
+                    document.createElement("<div id='underscoreBlameHtmlPage' style='display: none;'></div>")
+                );
+                // document.querySelector("#underscoreBlameHtmlPage").innerHTML = html;
+
             
             });
+    }
+}
+setInterval(() => {
+    latestUrl = window.location.href
+    if(latestUrl!=URL) {
+        URL = latestUrl
+        changed()
     }
 }, 500);
