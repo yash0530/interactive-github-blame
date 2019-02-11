1d87ec4b64a4f1b4ef9498ebbb7578c61247ff77
Aliabbas Merchant <merchantaliabbas@gmail.com>
2019-02-11 23:30:48 +0530

    Completed blame data extraction

--- blame.js
+++ blame.js
@@ -15,16 +15,34 @@ function set_dom(html) {
 }
 function blameDetails(line_id) {
     let super_parent = container.querySelector("#"+line_id).parentNode.parentNode.parentNode;
+    // blame_commit = super_parent.querySelector(".blame-commit").querySelector(".flex-items-start")
     blame_commit = super_parent.querySelector(".blame-commit")
-    blame_reblame = super_parent.querySelector(".blame-reblame")
 
-    // WIP
-    // all will be nested dictionaries
+    avatar = blame_commit.querySelector(".avatar")
+    user_link = "https://github.com" + avatar.getAttribute("href")
+    avatar_src = avatar.querySelector("img").getAttribute("src")
+
+    content = blame_commit.querySelector(".message")
+    commit_msg = content.innerText
+    commit_link = "https://github.com" + content.getAttribute("href")
+
+    time = blame_commit.querySelector("time-ago").getAttribute("datetime")
+    time = new Date(time).toString()
+
+    let reblame = ''
+    try {
+        reblame = "https://github.com" + super_parent.querySelector(".blob-reblame").querySelector("a").getAttribute("href")
+        reblame = reblame.replace("blame", "blob")
+    } catch (error) {
+    }
+    
     let data = {
-        'avatar':'avatar',
-        'commit':'commit',
-        'time':'time',
-        'reblame':'reblame'
+        avatar_src,
+        user_link,
+        commit_msg,
+        commit_link,
+        time,
+        reblame
     }
     return data
 }
