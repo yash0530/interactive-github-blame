0c281413ec136c2d479ff8a8787ccefb96cf0ca9
yash0530 <yashjain0530@gmail.com>
2019-02-11 16:00:44 +0530

    _

--- content.js
+++ content.js
@@ -1,6 +1,24 @@
-var style = document.createElement('link');
-style.rel = 'stylesheet';
-style.type = 'text/css';
-style.href = chrome.extension.getURL('styles.css');
+let latestUrl = window.location.href;
 
-console.log("FROM EXTENSION");
+setInterval(() => {
+
+    if (latestUrl === window.location.href && 
+        window.location.href.indexOf("/blob/") !== -1) {
+
+        latestUrl = window.location.href;
+
+        console.log("REACHED");
+        
+        // getting the html of blame page 
+        const url = window.location.href.replace("/blob/", "/blame/");
+
+        fetch(url)
+            .then(data => data.text())
+            .then(data => {
+                console.log(data);
+                
+                // TODO
+            
+            });
+    }
+}, 500);
